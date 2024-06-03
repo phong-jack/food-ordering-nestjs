41e82dc08d44349d00808ac6d76e1bcba737289f
JackPhong <ngotienphong5141@gmail.com>
2024-06-03 11:41:57 +0700

    test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
+  console.log('test');
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
